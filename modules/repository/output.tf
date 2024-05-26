6fe009f0609fc2ed836e4066eef8af8e0774f2ca
田中雄大 <take@mactake.local>
2024-05-26 22:05:17 +0900

    コメント

--- modules/repository/output.tf
+++ modules/repository/output.tf
@@ -3,11 +3,11 @@ output "github_pubnlic_repos" {
   value = [
     // Public
     {repo_name = "github_terraform", visibility = "public" , archived = false , description = "GithubをTerraformで管理するためのリポジトリ"},
-    {repo_name = "blog", visibility = "public" , archived = false , description = ""},
-    {repo_name = "docker", visibility = "public" , archived = false , description = ""},
-    {repo_name = "ecs-learning-course", visibility = "public" , archived = false , description = ""},
+    {repo_name = "blog", visibility = "public" , archived = false , description = "zennの記事を管理するためのリポジトリ"},
+    {repo_name = "docker", visibility = "public" , archived = false , description = "Docker関連のファイルを管理"},
+    {repo_name = "ecs-learning-course", visibility = "public" , archived = false , description = "udemyのデプロイに関するサンプルコート"},
 
-    # // Private
-    {repo_name = "aws_terraform", visibility = "private" , archived = false , description = ""},
+    // Private
+    {repo_name = "aws_terraform", visibility = "private" , archived = false , description = "AWSリソースをTerraformで管理するためのリポジトリ"},
   ]
 }
