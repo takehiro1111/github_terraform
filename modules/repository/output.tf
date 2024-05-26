0b58615119c1b26a202098d841706b2fdea0932b
田中雄大 <take@mactake.local>
2024-05-27 00:54:01 +0900

    repo-add

--- modules/repository/output.tf
+++ modules/repository/output.tf
@@ -6,6 +6,7 @@ output "github_pubnlic_repos" {
     {repo_name = "blog", visibility = "public" , archived = false , description = "zennの記事を管理するためのリポジトリ"},
     {repo_name = "docker", visibility = "public" , archived = false , description = "Docker関連のファイルを管理"},
     {repo_name = "ecs-learning-course", visibility = "public" , archived = false , description = "udemyのデプロイに関するサンプルコート"},
+    {repo_name = "python", visibility = "public" , archived = false , description = "pythonに関するコードを管理するリポジトリ"},
 
     // Private
     {repo_name = "aws_terraform", visibility = "private" , archived = false , description = "AWSリソースをTerraformで管理するためのリポジトリ"},
