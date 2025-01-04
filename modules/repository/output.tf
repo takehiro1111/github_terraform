79903812fb887a8dc73c723b86f731d6943a6cb7
takehiro1111 <t.soccer.0916@gmail.com>
2025-01-05 00:17:37 +0900

    add：new repository

--- modules/repository/output.tf
+++ modules/repository/output.tf
@@ -17,5 +17,6 @@ output "github_pubnlic_repos" {
     {repo_name = "prometheus", visibility = "public" , archived = false , description = "Prometheusの監視基盤の構築"},
     {repo_name = "locust", visibility = "public" , archived = false , description = "負荷試験ツールのLocustを構築"},
     {repo_name = "k8s", visibility = "public" , archived = false , description = "Kubernetes関連の設定"},
+    {repo_name = "node", visibility = "public" , archived = false , description = "Node.jsを用いた実装"},
   ]
 }
