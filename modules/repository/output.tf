f763693bd1030d92cbdbf4cd452527eff6bab74c
takehiro1111 <t.soccer.0916@gmail.com>
2025-10-14 22:54:06 +0900

    add:repository

--- modules/repository/output.tf
+++ modules/repository/output.tf
@@ -30,5 +30,6 @@ output "github_pubnlic_repos" {
     {repo_name = "menta-react-next", visibility = "private" , archived = false , description = "", allow_auto_merge = false},
     {repo_name = "cdk", visibility = "public" , archived = false , description = "", allow_auto_merge = false},
     {repo_name = "dev-personal-log", visibility = "public" , archived = false , description = "プライベートで触りたい技術スタックや学習ログ", allow_auto_merge = false},
+    {repo_name = "takehiro1111", visibility = "public" , archived = false , description = "Profile", allow_auto_merge = false},
   ]
 }
